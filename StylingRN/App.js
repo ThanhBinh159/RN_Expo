1bac4e95e78718ba782581666acfc6e00a3cc792
Thanh bình <binhnt1509204@gmail.com>
2025-08-30 16:04:32 +0700

    demo styling

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -4,8 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>StyleSheet API</Text>
+      <View style={[styles.box, styles.lightBlueBox, styles.iosShadow]}>
+        <Text>LightBlue box</Text>
+      </View>
+      <View style={[styles.box, styles.lightGreenBox, styles.androidShadow]}>
+        <Text>LightGreen box</Text>
+      </View>
     </View>
   );
 }
@@ -14,7 +19,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 60,
   },
+  box:{
+    width: 200,
+    height: 200,
+    // padding: 10,
+    paddingHorizontal: 10, // left and right
+    paddingVertical: 20, // top and bottom
+    // margin: 20,
+    marginHorizontal: 20, // left and right
+    marginVertical: 10, // top and bottom
+    //border
+    borderWidth: 2,
+    borderColor: "purple",
+    borderRadius: 10,
+  },
+  lightBlueBox: {
+    backgroundColor: 'lightblue',
+  },
+  lightGreenBox: {
+    backgroundColor: 'lightgreen',
+  },
+  iosShadow:{ //only can use for IOS
+    shadowColor: "#333333",
+    shadowOffset:{
+      width:6,
+      height: 6
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+  },
+  androidShadow:{
+    elevation: 10,
+  }
 });
